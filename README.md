34442573b2e72653639a6053a4d98c42bfd0e065
Edward <78192184+F8TGT@users.noreply.github.com>
2024-05-19 19:38:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Laravel-Tasks-List
+ 
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function() {
+
+});
+
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks' => \App\Models\Task::latest()->get()
+    ]);
+})->name('tasks.index');
+
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', ['task' =>  \App\Models\Task::findOrFail($id)]);
+})->name('tasks.show');
+
+Route::post('/tasks', function(Request $request) {
+    dd($request->all());
+})->name('tasks.store');
